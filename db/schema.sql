3e34fc0b83a3875458426c92bd6b84516ea3169e
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-22 14:49:40 -0500

    added query and schema, started to add seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,33 @@
 -- drop if exist, then create, then USE
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
 -- create tables for department, role, and employee
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+-- role
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+-- employee
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT, 
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,20 @@
 --populate database
---Insert into: Department, role, and employee 
+INSERT INTO department (name)
+--Values youll be inserting into tables below
+VALUES ("Engineering"),
+        ("Communications"),
+        ("Legal"),
+        ("Accounting");
+--Insert into: Department, role, and employee
+-- role: title, salary, dept_id
+INSERT INTO role (title, salary, department_id)
+--random data to insert into db based on positioned values
+VALUES ("Software Engineer", 150000, 1),
+        ("Lead Engineer", 200000, 1),
+        ("Communications Lead", 70000, 2),
+        ("Communications Assistant", 40000, 2),
+        ("Lawyer", 300000, 3),
+        ("Paralegal", 200000, 3),
+        ("Bookkeeper", 70000, 4),
+        ("Controller", 100000, 4);
+        
