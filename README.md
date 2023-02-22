b3e97636e90646888833fa190dbc217e84da4d18
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-22 16:05:04 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # emptracker
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+This application is a system used view and udpate employee information using Node, Inquirer and MySQL. [video](https://drive.google.com/file/d/1o6JRaSK95NKJTWJaTTAQkxZRsLb8nlKF/view)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+
+## Installation
+
+To install the application, first require the necessary packages by running npm i. Then using a MySQL shell, create the database, tables and seed the data.
+
+## Usage
+
+To use the application, start the server by running node index.js in the command line. Then toggle through various options to view, interact with and update the employee database.
+
+## Credits
+
+Collaborators: Sha Chambers
+
+## License
+
+This application is covered under the MIT license.
+
+## Contributing
+
+N/A
+
+## Tests
+
+N/A
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-//import required things
-// Be sure to create ENV file or none of this will work!!!
+// Require necessary packages
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
-//require env
 require("dotenv").config();
 
-// database connection that goes thru env user, pw, and name
+// Connect to db
 const db = mysql.createConnection({
   host: "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
-//Questions prompt in terminal
+
+//  Questions array for prompt
 const options = [
   {
     type: "list",
@@ -31,98 +30,153 @@ const options = [
     name: "options",
   },
 ];
-//async function to get you to query db
-//Think try/catch
-//conditional logic within the try before you catch an error
-async function init(){
-try {
-    inquirer
-    //start the prompt
-    //Write SQL syntax as the awaited promised result
-    .prompt(options)
-    .then(async (res)=> {
-        if ('view all departments' === res.options) {
-            const[results] = await db.promise.()query(`SELECT * FROM department`)
-            console.table(results);
-            return init();
-        } else if ('view all roles' === res.options) {
-            const [results] = await db.promise().query(`SELECT role.title, role.id, role.salary, department.name AS department_id
-            FROM role
-            JOIN department ON role.department_id = department_id;`);
-            console.table(results);
-            return init();
 
-        }else if ('view all employees' === res.options){
-            const [results] = await db.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, role.salary, role.title AS role, department.name AS department, CONCAT(manager.first_name, "_", manager.last_name) AS manager
-            FROM employee
-            JOIN role ON employee.role_id = role.id
-            JOIN department ON role.department_id = department.id
-            LEFT JOIN employee manager ON  manager.id = employee.manager_id;`)
-            console.table(results);
-            return init();
-        } else if ('Add a department' === res.options){
-            const res = await inquirer.prompt([{
-                type: 'input',
-                message: 'enter department name:',
-                name: 'newDepartment',
-        }
-    ])
-    const [results] = await db.promise().query(`INSERT INTO department(name) VALUES (?)`, res.newDepartment)
-                    console.table(results);
-                    return init();
-        }
-        //Add a role
-        else if ('Add a role' === res.options) {
-            const [departments] = await db.promise().query(`SELECT name, id AS value FROM department`)
-            //prompt user to input info on role
-            const res = await inquirer.prompt([{
-                type: 'input',
-                message: 'Enter job title:',
-                name: 'title',
-            },
-            {
-                type: 'input',
-                message: 'Enter salary:',
-                name: 'salary',
-            },
-            {
-                type: 'list',
-                message: 'Select a department:',
-                choices: departments,
-                name: 'department'
-            },
-        ]);
-              //insert into roles using empty values in a promise query
-        const [results] = await db.promise().query(`INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)`, [res.title, res.salary, res.department])
+// Function to prompt user and query to db
+async function init() {
+  try {
+    inquirer.prompt(options).then(async (res) => {
+      if ("View all departments" === res.options) {
+        const [results] = await db.promise().query(`SELECT * FROM department`);
         console.table(results);
         return init();
-    } else if ('Update an employee role' === res.options) {
-        const [employee] = await db.promise().query(`SELECT CONCAT(first_name, "_", last_name) AS name, id AS value FROM employee`)
-        const [role] = await db.promise().query(`SELECT title AS name, id AS value FROM role`)
+      } else if ("View all roles" === res.options) {
+        const [results] = await db.promise()
+          .query(`SELECT role.title, role.id, role.salary, department.name AS department_id
+                    FROM  role
+                    JOIN department ON role.department_id = department.id;`);
+        console.table(results);
+        return init();
+      } else if ("View all employees" === res.options) {
+        const [results] = await db.promise()
+          .query(`SELECT employee.id, employee.first_name, employee.last_name, role.salary, role.title AS role, department.name AS department, CONCAT(manager.first_name, "_", manager.last_name) AS manager
+                    FROM employee
+                    JOIN role ON employee.role_id = role.id
+                    JOIN department ON role.department_id = department.id
+                    LEFT JOIN employee manager ON  manager.id = employee.manager_id;`);
+        console.table(results);
+        return init();
+      } else if ("Add a department" === res.options) {
         const res = await inquirer.prompt([
-            {
-                type: 'list',
-                message: 'Select an employee:',
-                choices: employee,
-                name: 'employee'
-            },
-            {
-                type: 'list',
-                message: 'Select a role:',
-                choices: role,
-                name: 'role'
-            }
+          {
+            type: "input",
+            message: "Enter department name:",
+            name: "newDepartment",
+          },
         ]);
-        //use results to update
-        const [results] = await db.promise().query(`UPDATE employee SET role_id = ? WHERE id = ?`, [res.role, res.employee])
+        const [results] = await db
+          .promise()
+          .query(`INSERT INTO department(name) VALUES (?)`, res.newDepartment);
         console.table(results);
         return init();
-    } else {
+      } else if ("Add a role" === res.options) {
+        const [departments] = await db
+          .promise()
+          .query(`SELECT name, id AS value FROM department`);
+        const res = await inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter job title:",
+            name: "title",
+          },
+          {
+            type: "input",
+            message: "Enter salary:",
+            name: "salary",
+          },
+          {
+            type: "list",
+            message: "Select a department:",
+            choices: departments,
+            name: "department",
+          },
+        ]);
+        const [results] = await db
+          .promise()
+          .query(
+            `INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)`,
+            [res.title, res.salary, res.department]
+          );
+        console.table(results);
+        return init();
+      } else if ("Add an employee" === res.options) {
+        const [role] = await db
+          .promise()
+          .query(`SELECT title AS name, id AS value FROM role`);
+        const [manager] = await db
+          .promise()
+          .query(
+            `SELECT CONCAT(first_name, "_", last_name) AS name, manager_id AS value FROM employee`
+          ); // query not working
+        const res = await inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter first name:",
+            name: "firstName",
+          },
+          {
+            type: "input",
+            message: "Enter last name:",
+            name: "lastName",
+          },
+          {
+            type: "list",
+            message: "Select a role",
+            choices: role,
+            name: "role",
+          },
+          {
+            type: "list",
+            message: "Select a manager",
+            chocies: manager,
+            name: "manager",
+          },
+        ]);
+        const [results] = await db
+          .promise()
+          .query(
+            `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+            [res.first_name, res.last_name, res.role, res.manager]
+          );
+        console.table(results);
+        return init();
+      } else if ("Update an employee role" === res.options) {
+        const [employee] = await db
+          .promise()
+          .query(
+            `SELECT CONCAT(first_name, "_", last_name) AS name, id AS value FROM employee`
+          );
+        const [role] = await db
+          .promise()
+          .query(`SELECT title AS name, id AS value FROM role`);
+        const res = await inquirer.prompt([
+          {
+            type: "list",
+            message: "Select an employee:",
+            choices: employee,
+            name: "employee",
+          },
+          {
+            type: "list",
+            message: "Select a role:",
+            choices: role,
+            name: "role",
+          },
+        ]);
+        const [results] = await db
+          .promise()
+          .query(`UPDATE employee SET role_id = ? WHERE id = ?`, [
+            res.role,
+            res.employee,
+          ]);
+        console.table(results);
+        return init();
+      } else {
         process.exit(0);
-    }
-    })
-} catch (err){
+      }
+    });
+  } catch (err) {
     console.log(err);
+  }
 }
-};
-init()
+
+init();
